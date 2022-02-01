a5b82428b7b672725aa9b34903b2bbb7a48416ab
densumesh <dens.sumesh79@gmail.com>
2022-02-01 07:58:02 -0700

    fix for update

--- src/__tests__/results.test.js
+++ src/__tests__/results.test.js
@@ -1,12 +1,19 @@
 import React from "react";
 import DisplayResults from "../components/DisplayResults.js";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import "jest-location-mock";
 import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 configure({ adapter: new Adapter() });
 it("can search without crashing", () => {
   window.location.assign("/search/test");
-  shallow(<DisplayResults />);
+  mount(
+    <Router>
+      <Routes>
+        <Route path="/search/:id" element={<DisplayResults />} />
+      </Routes>
+    </Router>
+  );
 });
